81e8db36e20fdd82cde9960c62e32049d7ee3688
Sebastian Dehne <sebastian@dehnes.com>
2020-12-03 16:35:17 +0100

    day 03 - cosmetics

--- src/com/dehnes/adventofcode_2020/day03/Day03.kt
+++ src/com/dehnes/adventofcode_2020/day03/Day03.kt
@@ -8,8 +8,6 @@ data class Point(
         val offset: Int
 )
 
-fun List<String>.isTree(pos: Point) = this[pos.line][pos.offset] == '#'
-
 fun List<String>.moveTo(current: Point, slope: Pair<Int, Int>) = (current.line + slope.first).let { newLineOffset ->
     if (newLineOffset < 0 || newLineOffset >= this.size) {
         null
@@ -18,12 +16,24 @@ fun List<String>.moveTo(current: Point, slope: Pair<Int, Int>) = (current.line +
     }
 }
 
+fun List<String>.calcTreesForSlope(slope: Pair<Int, Int>): Long {
+    var pos = Point(0, 0)
+    var treeCount = 0L
+    while (true) {
+        pos = this.moveTo(pos, slope) ?: break
+        treeCount += if (this[pos.line][pos.offset] == '#') 1 else 0
+    }
+    return treeCount
+}
+
 fun main() {
     val map = File("resources/day03.txt")
             .readLines(Charset.defaultCharset())
+    val calcTreesForSlope = { slope: Pair<Int, Int> ->
+        map.calcTreesForSlope(slope)
+    }
 
-    val resultPart1 = testSlope(map, 1 to 3)
-    println("Result part1: $resultPart1")
+    println("Result part1: ${calcTreesForSlope(1 to 3)}")
 
     val resultPart2 = listOf(
             1 to 1,
@@ -31,17 +41,6 @@ fun main() {
             1 to 5,
             1 to 7,
             2 to 1
-    ).map { pair -> testSlope(map, pair) }.reduce { acc, i -> acc * i }
+    ).map(calcTreesForSlope).reduce { acc, i -> acc * i }
     println("Result part2: $resultPart2")
-}
-
-private fun testSlope(map: List<String>, slope: Pair<Int, Int>): Long {
-    var pos = Point(0, 0)
-    var treeCount = 0L
-    while (true) {
-        pos = map.moveTo(pos, slope) ?: break
-        if (map.isTree(pos))
-            treeCount += 1
-    }
-    return treeCount
 }
