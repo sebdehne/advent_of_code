0a5710ba42a1493f74700a1275910718a50d4fc2
Sebastian Dehne <sebastian@dehnes.com>
2020-12-07 21:21:27 +0100

    day 07 - shorter

--- src/com/dehnes/adventofcode_2020/day07/Day07.kt
+++ src/com/dehnes/adventofcode_2020/day07/Day07.kt
@@ -2,34 +2,25 @@ package com.dehnes.adventofcode_2020.day07
 
 import java.io.File
 
-data class BagRule(
-        val color: String,
-        val contains: List<Pair<Int, String>>
-)
+val bagRules = File("resources/day07.txt").readLines().map { ruleStr ->
+    ruleStr.split(" ", ",", ".").filter(String::isNotBlank).let { split ->
+        "${split[0]} ${split[1]}" to (4 until split.size step 4).map {
+            split[it].replace("no", "0").toInt() to "${split[it + 1]} ${split[it + 2]}"
+        }.filter { p -> p.first > 0 }
+    }
+}.toMap()
 
-fun uniqueParents(map: List<BagRule>, rule: BagRule): Set<String> = map
-        .filter { other -> other.contains.any { it.second == rule.color } }.map { parent -> uniqueParents(map, parent) }
+fun uniqueParents(rules: Map<String, List<Pair<Int, String>>>, color: String): Set<String> = rules
+        .filter { other -> other.value.any { it.second == color } }.map { parent -> uniqueParents(rules, parent.key) }
         .flatten()
-        .toSet() + rule.color
+        .toSet() + color
 
-fun childrenInstances(map: List<BagRule>, rule: BagRule): Long =
-        1 + rule.contains.map { child -> child.first * childrenInstances(map, map.first { r -> r.color == child.second }) }.sum()
+fun childrenInstances(rules: Map<String, List<Pair<Int, String>>>, color: String): Long =
+        1 + rules[color]!!.map { child -> child.first * childrenInstances(rules, child.second) }.sum()
 
 fun main() {
-    val bagRules = File("resources/day07.txt").readLines().map { ruleStr ->
-        ruleStr.split(" ", ",", ".").filter(String::isNotBlank).let { split ->
-            BagRule(
-                    "${split[0]} ${split[1]}",
-                    (4 until split.size step 4).map {
-                        split[it].replace("no", "0").toInt() to "${split[it + 1]} ${split[it + 2]}"
-                    }.filter { p -> p.first > 0 }
-            )
-        }
-    }
-
     val targetColor = "shiny gold"
-    val targetRule = bagRules.first { r -> r.color == targetColor }
 
-    println("Part1: ${(uniqueParents(bagRules, targetRule) - targetColor).size}") // 139
-    println("Part2: ${childrenInstances(bagRules, targetRule) - 1}") // 58175
+    println("Part1: ${(uniqueParents(bagRules, targetColor) - targetColor).size}") // 139
+    println("Part2: ${childrenInstances(bagRules, targetColor) - 1}") // 58175
 }
