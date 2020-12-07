72be90405c8bcd6b890e3b33acb6ab6a3ad8e773
Sebastian Dehne <sebastian@dehnes.com>
2020-12-07 22:21:50 +0100

    day 02

--- src/com/dehnes/adventofcode_2020/day02/Day02.kt
+++ src/com/dehnes/adventofcode_2020/day02/Day02.kt
@@ -1,49 +1,27 @@
 package com.dehnes.adventofcode_2020.day02
 
 import java.io.File
-import java.nio.charset.Charset
 
-data class PasswordPolicy(
-        val start: Int,
-        val end: Int,
-        val char: Char
-)
-
-fun String.isValidPasswordPart1(policy: PasswordPolicy) = IntRange(policy.start, policy.end).contains(this
-        .filter { it == policy.char }
+fun isValidPasswordPart1(passwordToPolicy: Pair<String, Pair<IntRange, Char>>) = passwordToPolicy.second.first.contains(passwordToPolicy.first
+        .filter { it == passwordToPolicy.second.second }
         .count()
 )
 
-fun String.isValidPasswordPart2(policy: PasswordPolicy): Boolean {
-    val isFirst = this.getOrNull(policy.start - 1) == policy.char
-    val isSecond = this.getOrNull(policy.end - 1) == policy.char
+fun isValidPasswordPart2(passwordToPolicy: Pair<String, Pair<IntRange, Char>>): Boolean {
+    val isFirst = passwordToPolicy.first.getOrNull(passwordToPolicy.second.first.first - 1) == passwordToPolicy.second.second
+    val isSecond = passwordToPolicy.first.getOrNull(passwordToPolicy.second.first.last - 1) == passwordToPolicy.second.second
     return (isFirst && !isSecond) || (!isFirst && isSecond)
 }
 
-fun main() {
-    val pattern = "(\\d+)-(\\d+)\\s+(\\S):\\s+(\\S+)".toRegex()
-    val passwords = File("resources/day02.txt")
-            .readLines(Charset.defaultCharset())
-            .map { line ->
-                pattern.findAll(line)
-                        .toList()
-                        .flatMap { matchResult -> matchResult.groupValues }
-                        .let { parts ->
-                            PasswordPolicy(
-                                    parts[1].toInt(),
-                                    parts[2].toInt(),
-                                    parts[3][0]
-                            ) to parts[4]
-                        }
+val pattern = "(\\d+)-(\\d+)\\s+(\\S):\\s+(\\S+)".toRegex()
+val passwords = File("resources/day02.txt").readLines().map {
+    pattern.findAll(it).toList().flatMap { matchResult -> matchResult.groupValues }
+            .let { parts ->
+                parts[4] to (parts[1].toInt()..parts[2].toInt() to parts[3][0])
             }
+}
 
-    val part1 = passwords
-            .filter { it.second.isValidPasswordPart1(it.first) }
-            .count()
-    println("Result part1: $part1")
-
-    val part2 = passwords
-            .filter { it.second.isValidPasswordPart2(it.first) }
-            .count()
-    println("Result part2: $part2")
+fun main() {
+    println("Part1: ${passwords.filter(::isValidPasswordPart1).count()}")
+    println("Part1: ${passwords.filter(::isValidPasswordPart2).count()}")
 }
