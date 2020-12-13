f346f556529f4fff702ca86075f0a797d5e3f24e
Sebastian Dehne <sebastian@dehnes.com>
2020-12-13 15:58:34 +0100

    day 13

--- src/com/dehnes/adventofcode_2020/day13/Day13.kt
+++ src/com/dehnes/adventofcode_2020/day13/Day13.kt
@@ -1,40 +1,23 @@
 package com.dehnes.adventofcode_2020.day13
 
-val input = "29,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,41,x,x,x,x,x,x,x,x,x,521,x," +
-        "x,x,x,x,x,x,23,x,x,x,x,13,x,x,x,17,x,x,x,x,x,x,x,x,x,x,x,x,x,601,x,x,x,x,x," +
-        "37,x,x,x,x,x,x,x,x,x,x,x,x,19"
+val input = (
+        "29,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,41,x,x,x,x,x,x,x,x,x,521,x,x,x,x,x,x,x,23,x,x,x,x,13,x,x,x,17,x,x,x,x,x,x,x,x,x,x,x,x,x,601,x,x,x,x,x,37,x,x,x,x,x,x,x,x,x,x,x,x,19"
+        ).split(",").map { if (it == "x") -1 else it.toLong() }
 
 fun main() {
     val timestamp = 1002461
-    val (buss, timeToWait) = input.split(",").filterNot { it == "x" }
-            .map { it.toInt() }
-            .map { b -> b to b - (timestamp % b) }.minByOrNull { it.second }!!
+    val (buss, timeToWait) = input.filter { it != -1L }.map { b -> b to b - (timestamp % b) }.minByOrNull { it.second }!!
     println("Part1: ${buss * timeToWait}") // 4207
 
-    val busses = input.split(",")
-            .mapIndexed { index, s ->
-                (if (s == "x") -1 else s.toLong()) to index.toLong()
-            }
-            .filter { it.first >= 0 }
-            .fold(1L to 0L) { acc, b ->
-                lcd(acc.first, b.first) to alignOffset(acc.second, acc.first, b.first, b.second)
-            }
-
-    println("Part2: ${busses.second}") // 725850285300475
+    val alignAt = input.mapIndexed { index, s -> s to index.toLong() }.filter { it.first >= 0 }.fold(1L to 0L) { acc, b ->
+        lcd(acc.first, b.first) to alignAt(acc.second, acc.first, b.first, b.second)
+    }.second
+    println("Part2: $alignAt") // 725850285300475
 }
 
-fun alignOffset(initOffset: Long, n1: Long, n2: Long, n2Delta: Long): Long {
-    var rounds = 0
-    var time: Long
-    while (true) {
-        time = rounds * n1 + initOffset
-        if ((time + n2Delta) % n2 == 0L) {
-            break
-        }
-        rounds++
-    }
-    return time
-}
+fun alignAt(initOffset: Long, n1: Long, n2: Long, n2Delta: Long): Long = generateSequence(0) { it + 1 }
+        .map { it * n1 + initOffset }
+        .first { (it + n2Delta) % n2 == 0L }
 
-fun lcd(n1: Long, n2: Long): Long = (n1 * n2) / hcf(n1, n2)
-fun hcf(n1: Long, n2: Long): Long = if (n2 != 0L) hcf(n2, n1 % n2) else n1
+fun lcd(n1: Long, n2: Long): Long = (n1 * n2) / gcd(n1, n2)
+fun gcd(n1: Long, n2: Long): Long = if (n2 != 0L) gcd(n2, n1 % n2) else n1
