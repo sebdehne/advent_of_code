591c81a5589d00302e11a7151e3d3596b5aa3d5b
Sebastian Dehne <sebastian@dehnes.com>
2020-12-15 07:08:36 +0100

    day 15

---
+++ src/com/dehnes/adventofcode_2020/Day15.kt
@@ -0,0 +1,25 @@
+package com.dehnes.adventofcode_2020
+
+import org.junit.jupiter.api.Test
+
+class Day15 {
+
+    @Test
+    fun test() {
+        val start = listOf(0L, 1, 4, 13, 15, 12, 16)
+
+        println(play(start, 2020)) // 1665
+        println(play(start, 30000000)) // 16439
+    }
+
+    private fun play(start: List<Long>, stopAt: Long) = start
+            .mapIndexed { index, l -> l to listOf(index + 1L) }.toMap()
+            .toMutableMap().let { spokenAt ->
+                (start.size + 1..stopAt).fold(start.last()) { prev, turn ->
+                    val turns = spokenAt[prev]
+                    val newV = if (turns?.size != 2) 0 else turns[0] - turns[1]
+                    spokenAt[newV] = listOfNotNull(turn, spokenAt[newV]?.getOrNull(0))
+                    newV
+                }
+            }
+}
