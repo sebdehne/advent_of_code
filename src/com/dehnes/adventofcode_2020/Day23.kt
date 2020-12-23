c21e03e7b74ceb2d650ea1469b9b8b0f4a1e1737
Sebastian Dehne <sebastian@dehnes.com>
2020-12-23 20:40:12 +0100

    Day23 - use array istead og hash

--- src/com/dehnes/adventofcode_2020/Day23.kt
+++ src/com/dehnes/adventofcode_2020/Day23.kt
@@ -6,9 +6,9 @@ class Day23 {
 
     @Test
     fun part1Array() {
-        val input = "614752839"
+        val input = "389125467"
         val list = input.toList().map { it.toString().toInt() }
-        val result = play(list, 100, 8)
+        val result = play(list, 10, 8)
         println(result.joinToString("")) // 89372645
     }
 
@@ -29,16 +29,21 @@ class Day23 {
 
 
     private fun play(input: List<Int>, moves: Int, resultLength: Int): List<Int> {
-        val cups = makeCircle(input)
         val max = input.maxOrNull()!!
+
+        val cupToNextNeighbor = IntArray(input.size + 1)
+        input.windowed(2) { (a, b) ->
+            cupToNextNeighbor[a] = b
+        }
+        cupToNextNeighbor[input.last()] = input.first()
+        val pickedUp = IntArray(3)
 
         var current = input.first()
         repeat(moves) {
-            val pickedUp = IntArray(3)
-            pickedUp[0] = cups[current]!!
-            pickedUp[1] = cups[pickedUp[0]]!!
-            pickedUp[2] = cups[pickedUp[1]]!!
-            cups[current] = cups[pickedUp[2]]!!
+            pickedUp[0] = cupToNextNeighbor[current]
+            pickedUp[1] = cupToNextNeighbor[pickedUp[0]]
+            pickedUp[2] = cupToNextNeighbor[pickedUp[1]]
+            cupToNextNeighbor[current] = cupToNextNeighbor[pickedUp[2]]
 
             var dstValue = current - 1
             while (true) {
@@ -51,31 +56,13 @@ class Day23 {
                 }
             }
 
-            cups[pickedUp[2]] = cups[dstValue]!!
-            cups[dstValue] = pickedUp[0]
-
-            current = cups[current]!!
+            cupToNextNeighbor[pickedUp[2]] = cupToNextNeighbor[dstValue]
+            cupToNextNeighbor[dstValue] = pickedUp[0]
+            current = cupToNextNeighbor[current]
         }
 
-        return (1 until resultLength).fold(listOf(cups[1]!!)) { acc, _ ->
-            acc + cups[acc.last()]!!
+        return (1 until resultLength).fold(listOf(cupToNextNeighbor[1])) { acc, _ ->
+            acc + cupToNextNeighbor[acc.last()]
         }
     }
-
-    private fun makeCircle(l: List<Int>): HashMap<Int, Int> {
-        val h = HashMap<Int, Int>()
-        val headValue = l[0]
-        h[headValue] = headValue
-
-        var lastInserted = headValue
-        (1 until l.size).forEach { i ->
-            val value = l[i]
-            h[value] = headValue
-            h[lastInserted] = value
-            lastInserted = value
-        }
-
-        return h
-    }
-
 }
