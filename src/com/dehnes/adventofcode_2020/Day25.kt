1e1b6d5769712f5bc4c5ad3d9425789be48bc95b
Sebastian Dehne <sebastian@dehnes.com>
2020-12-25 09:51:21 +0100

    Day25

---
+++ src/com/dehnes/adventofcode_2020/Day25.kt
@@ -0,0 +1,26 @@
+package com.dehnes.adventofcode_2020
+
+import org.junit.jupiter.api.Test
+
+class Day25 {
+
+    @Test
+    fun part1() {
+        val input1 = 1965712L
+        val input2 = 19072108L
+
+        val cardLoopSize = findLoopSize(input1, 7)
+        println((1..cardLoopSize).fold(1L) { acc, _ ->
+            (acc * input2) % 20201227
+        })// 16881444
+    }
+
+    fun findLoopSize(publicKey: Long, subject: Long): Int {
+        var value = 1L
+        return generateSequence(1) { it + 1 }.first {
+            value = (value * subject) % 20201227
+            value == publicKey
+        }
+    }
+
+}
